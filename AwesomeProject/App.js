984648b4db2acfd387dc7e4671dd56dafbbe0f2b
Lucius-Nguyen <nhatccg456@gmail.com>
2023-09-19 19:56:07 +0700

    hahahahaha

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -22,7 +22,7 @@ import { StyleLogin } from './src/css/Styles';
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={StyleLogin.header}>nhat-commit</Text>
+      <Text style={StyleLogin.header}>nhatfsadfa-commit</Text>
     </SafeAreaView>
   )
 }
