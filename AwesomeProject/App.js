ed5f8b96a6b173aba5daa0f59d0349c8d557be74
Quý Lê <101096483+quyle0051@users.noreply.github.com>
2023-09-21 22:01:26 +0700

    navigator

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,5 +1,4 @@
 
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -12,49 +11,23 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
-import Icon1 from 'react-native-vector-icons/Ionicons'
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { StyleLogin } from './src/css/Styles';
 import Home from './src/screens/Home';
 import Search from './src/screens/Search';
 import Order from './src/screens/Order';
 import Login from './src/screens/Login';
-
-const Tab = createBottomTabNavigator();
+import { AppContextProvider } from './src/utils/AppContext';
+import AppNavigator from './src/utils/AppNavigator';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarShowLabel: true,
-          tabBarHideOnKeyboard: true,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline'
-            } else if (route.name === 'Search') {
-              iconName = focused ? 'search-sharp' : 'search-outline'
-            }else if (route.name === 'Order') {
-              iconName = focused ? 'bag-handle-sharp' : 'bag-handle-outline'
-            }else if (route.name === 'Login') {
-              iconName = focused ? 'people-sharp' : 'people-outline'
-            }
-            return <Icon1 name= {iconName} size={size} color={color} />
-          },
-          tabBarActiveTintColor: '#3669C9',
-          tabBarInactiveTintColor: 'black',
-        })}
-      >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Search" component={Search} />
-        <Tab.Screen name="Order" component={Order} />
-        <Tab.Screen name="Login" component={Login} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <AppContextProvider>
+      <NavigationContainer>
+        <AppNavigator/>
+      </NavigationContainer>
+    </AppContextProvider>
   )
 }
 
