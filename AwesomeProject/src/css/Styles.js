06b7b20ce18d839b9667052f4ca2768e87036bcf
nguyenvantin1137 <117248091+nguyenvantin1137@users.noreply.github.com>
2023-10-04 17:32:11 +0700

    stylechange

--- AwesomeProject/src/css/Styles.js
+++ AwesomeProject/src/css/Styles.js
@@ -1,167 +1,157 @@
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
-import {COLOR} from './Theme';
-import {Dimensions} from 'react-native';
-const {width, height} = Dimensions.get('screen');
+import { COLOR } from './Theme';
+import { Dimensions } from 'react-native';
+const { width, height } = Dimensions.get('screen');
 export const StyleLogin = StyleSheet.create({
-  header: {
-    color: COLOR.primary,
-  },
+    header: {
+        color: COLOR.primary,
+    },
 });
-
-
-
-
-
-
 export const stylesNoResult = StyleSheet.create({
     view: {
-        margin:10,
+        margin: 10,
         height: '90%',
-        justifyContent:'center',
-        alignItems:'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     image: {
 
     },
     text: {
-        margin:10,
-        fontSize:20,
-        textAlign:'center',
-        color:'black',
-        fontWeight:'bold'
+        margin: 10,
+        fontSize: 20,
+        textAlign: 'center',
+        color: 'black',
+        fontWeight: 'bold'
     },
-    lightText: {fontSize:16,},
+    lightText: { fontSize: 16, },
 });
 export const StyleSearchSuggestions = StyleSheet.create({
-  image: {
-    width: 22,
-    height: 22,
-    margin: 9,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    lineHeight: 20,
-    letterSpacing: 0.2,
-  },
-  WholeViewInItem: {
-    margin: 8,
-    flexDirection: 'row',
-    width: width * 0.9,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  viewInside: {
-    flexDirection: 'row',
-  },
-  text: {
-    fontSize: 19,
-    lineHeight: 20,
-    letterSpacing: 0.2,
-    alignSelf: 'center',
-  },
+    image: {
+        width: 22,
+        height: 22,
+        margin: 9,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        lineHeight: 20,
+        letterSpacing: 0.2,
+    },
+    WholeViewInItem: {
+        margin: 8,
+        flexDirection: 'row',
+        width: width * 0.9,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    viewInside: {
+        flexDirection: 'row',
+    },
+    text: {
+        fontSize: 19,
+        lineHeight: 20,
+        letterSpacing: 0.2,
+        alignSelf: 'center',
+    },
 });
-
-
 export const StyleHomeStore = StyleSheet.create({
-<<<<<<< HEAD
-  container: {
-  },
-  menu: {
-      paddingTop: 50,
-      flexDirection: 'row',
-      marginLeft: 30,
-      marginRight: 30,
-  },
-  textTitle: {
-      textAlign: 'center',
-      color: 'black',
-      fontSize: 20,
-      paddingLeft: 110,
-      marginTop: -6
-  },
-  images: {
-      marginLeft: 80
-  },
-  touchOpa: {
-      marginLeft: 75
-  },
-  line: {
-      borderWidth: 1,
-      opacity: 0.03,
-      marginTop: 20
-  },
-  boxSeller: {
-      marginTop: 30,
-      paddingLeft: 30,
-      paddingRight: 30,
-      flexDirection: 'row'
-  },
-  infoSeller: {
-      flexDirection: 'row',
-  },
-  boxNameSeller: {
-      width: 218,
-      marginLeft: 20
-  },
-  boxRate: {
-      flexDirection: 'row',
-      paddingTop: 10
-  },
-  textRate: {
-      fontSize: 20,
-      color: 'black',
-      marginTop: -5,
-      paddingLeft: 5
-  },
-  boxLocate: {
-      flexDirection: 'row',
-      marginLeft: 30,
-      marginTop: 20
-  },
-  boxProductStore: {
-      flexDirection: 'row',
-      marginTop: 20,
-      marginLeft: 30,
-      justifyContent: 'space-between',
-      marginRight: 30
-  },
-  textTitleProduct: {
-      fontSize: 15
-  },
-  textCountProduct: {
-      color: 'black',
-      fontSize: 13
-  },
-  textitleProductSelling: {
-      fontSize: 20,
-      color: 'black'
-  },
-  productSelling: {
-      marginTop: 15,
-      marginLeft: 30,
-      marginRight: 30
-  },
-  boxSelling: {
-      backgroundColor: 'FAFAFA',
-      paddingLeft: 30,
-      paddingRight: 30,
-  },
-  boxProduct: {
-      backgroundColor: 'white',
-      borderRadius: 10,
-      padding: 10,
-      margin: 3,
-  },
-  reviewsProduct: {
-      flexDirection:'row',
-      alignItems:'center',
-      marginTop:5
-  }
-=======
+
     container: {
     },
+    menu: {
+        paddingTop: 50,
+        flexDirection: 'row',
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    textTitle: {
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 20,
+        paddingLeft: 110,
+        marginTop: -6
+    },
+    images: {
+        marginLeft: 80
+    },
+    touchOpa: {
+        marginLeft: 75
+    },
+    line: {
+        borderWidth: 1,
+        opacity: 0.03,
+        marginTop: 20
+    },
+    boxSeller: {
+        marginTop: 30,
+        paddingLeft: 30,
+        paddingRight: 30,
+        flexDirection: 'row'
+    },
+    infoSeller: {
+        flexDirection: 'row',
+    },
+    boxNameSeller: {
+        width: 218,
+        marginLeft: 20
+    },
+    boxRate: {
+        flexDirection: 'row',
+        paddingTop: 10
+    },
+    textRate: {
+        fontSize: 20,
+        color: 'black',
+        marginTop: -5,
+        paddingLeft: 5
+    },
+    boxLocate: {
+        flexDirection: 'row',
+        marginLeft: 30,
+        marginTop: 20
+    },
+    boxProductStore: {
+        flexDirection: 'row',
+        marginTop: 20,
+        marginLeft: 30,
+        justifyContent: 'space-between',
+        marginRight: 30
+    },
+    textTitleProduct: {
+        fontSize: 15
+    },
+    textCountProduct: {
+        color: 'black',
+        fontSize: 13
+    },
+    textitleProductSelling: {
+        fontSize: 20,
+        color: 'black'
+    },
+    productSelling: {
+        marginTop: 15,
+        marginLeft: 30,
+        marginRight: 30
+    },
+    boxSelling: {
+        backgroundColor: 'FAFAFA',
+        paddingLeft: 30,
+        paddingRight: 30,
+    },
+    boxProduct: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 10,
+        margin: 3,
+    },
+    reviewsProduct: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 5
+    },
+
     menu: {
         paddingTop: 30,
         flexDirection: 'row',
@@ -290,7 +280,7 @@ export const StyleShipper = StyleSheet.create({
     textShippe: {
         marginLeft: 30
     }
->>>>>>> devTin
+
 })
 
 export const StyleSearch = StyleSheet.create({
@@ -319,33 +309,33 @@ export const StyleSearch = StyleSheet.create({
         marginTop: 20
     },
     inputSearch: {
-        paddingLeft:30,
-        paddingRight:30,
-        paddingTop:30,
-        flexDirection:'row',
-        alignItems:'center',       
-    },
-    viewSearchbar:{
-        backgroundColor: '#c0c0c03c',
-        alignSelf: 'center', 
-        width: '90%', 
+        paddingLeft: 30,
+        paddingRight: 30,
+        paddingTop: 30,
         flexDirection: 'row',
-        height:50,
-        marginLeft:30,
-        borderRadius:10,
-        marginRight:30,
-        marginTop:30,
-      }
+        alignItems: 'center',
+    },
+    viewSearchbar: {
+        backgroundColor: '#c0c0c03c',
+        alignSelf: 'center',
+        width: '90%',
+        flexDirection: 'row',
+        height: 50,
+        marginLeft: 30,
+        borderRadius: 10,
+        marginRight: 30,
+        marginTop: 30,
+    }
     ,
     textInput: {
-        padding:10,
-        width:'90%',
-        borderRadius:10,
-        fontSize:15
+        padding: 10,
+        width: '90%',
+        borderRadius: 10,
+        fontSize: 15
 
     },
     iconSearch: {
-        alignSelf:'center'
+        alignSelf: 'center'
     },
     boxSeller: {
         marginTop: 30,
@@ -405,72 +395,72 @@ export const StyleSearch = StyleSheet.create({
         margin: 3,
     },
     reviewsProduct: {
-        flexDirection:'row',
-        alignItems:'center',
-        marginTop:5
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 5
     },
     flatlistHistory: {
-        marginTop:30
+        marginTop: 30
     }
 });
-export const StyleBanner =  StyleSheet.create({
-    secondText:{
-                fontFamily: 'DMSans-VariableFont_opsz,wght',
-    },
-    title:{
+export const StyleBanner = StyleSheet.create({
+    secondText: {
         fontFamily: 'DMSans-VariableFont_opsz,wght',
-        fontWeight:'700',
-        fontSize:25,
-        lineHeight:35
-    },WholeView:{
-        borderRadius:15,
-        flexDirection:'row',
-        width:'86%',
-        alignSelf:'center',
-        padding:10,
-        marginLeft:30,
-        marginRight:30,
-        marginTop:30,
-        marginBottom:30,
+    },
+    title: {
+        fontFamily: 'DMSans-VariableFont_opsz,wght',
+        fontWeight: '700',
+        fontSize: 25,
+        lineHeight: 35
+    }, WholeView: {
+        borderRadius: 15,
+        flexDirection: 'row',
+        width: '86%',
+        alignSelf: 'center',
+        padding: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 30,
+        marginBottom: 30,
 
 
 
-    },icon:{
-        marginLeft:5
-    },image:{
-        alignSelf:'flex-end'
+    }, icon: {
+        marginLeft: 5
+    }, image: {
+        alignSelf: 'flex-end'
     }
 
- });
+});
 
- export const styleWelcome = StyleSheet.create({
-    view:{
-        backgroundColor:'#3669C9',
-        height:'100%',
-        width:'100%',
+export const styleWelcome = StyleSheet.create({
+    view: {
+        backgroundColor: '#3669C9',
+        height: '100%',
+        width: '100%',
     },
-    text:{
-        width:'83%',
-        fontFamily:'DMSans-VariableFont_opsz,wght',
-        fontSize:43,
-        fontWeight:'bold',
-        color:'white',
-        margin:20
+    text: {
+        width: '83%',
+        fontFamily: 'DMSans-VariableFont_opsz,wght',
+        fontSize: 43,
+        fontWeight: 'bold',
+        color: 'white',
+        margin: 20
     },
-    TextInput:{
-        backgroundColor:'white',
-        width:'80%',
-        alignSelf:'center',
-        justifyContent:'center',
-        height:50,
-        marginBottom:50,
-        borderRadius:30
+    TextInput: {
+        backgroundColor: 'white',
+        width: '80%',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        height: 50,
+        marginBottom: 50,
+        borderRadius: 30
     },
-    MiniTextInput:{
+    MiniTextInput: {
         // color : '#EE2624',
-        color : '#3669C9',
-        textAlign:'center',
-        fontSize:25,
-        fontWeight:'bold'
+        color: '#3669C9',
+        textAlign: 'center',
+        fontSize: 25,
+        fontWeight: 'bold'
     }
- });
+});
