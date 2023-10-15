bd520d495621fbb850e22e28faf4f9cad247ff9a
Lucius-Nguyen <nhatccg456@gmail.com>
2023-10-15 22:14:43 +0700

    nhat-commit

--- AwesomeProject/src/component/SearchSuggestions/FetchSearchSuggestion.js
+++ AwesomeProject/src/component/SearchSuggestions/FetchSearchSuggestion.js
@@ -1,22 +1,12 @@
 export const fetchingData = () => {
     const data = [
-        "Dcm ao that day",
-        "Aiya kkk",
-        "Damn man fuck this shit",
-        "Dcm ao that dayaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddddddddddddddddddddddddddddd",
-        "Aiya kkk",
-        "Damn man fuck this shit","Dcm ao that day",
-        "Aiya kkk",
-        "Damn man fuck this shit","Dcm ao that day",
-        "Aiya kkk",
-        "Damn man fuck this shit","Dcm ao that day",
-        "Aiya kkk",
-        "Damn man fuck this shit","Dcm ao that day",
-        "Aiya kkk",
-        "Damn man fuck this shit","Dcm ao that day",
-        "Aiya kkk",
-        "Damn man fuck this shit",
-        
+        "Ao hoodie",
+        "Quan tay",
+        "Ao so mi",
+        "Kinh mat",
+        "tai nghe",
+        "but chi",
+        "Trang suc"
 
     ];
     
