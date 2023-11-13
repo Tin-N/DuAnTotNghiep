bf36b87595022ac5787fc55c99093936b80fa5cf
Nguyen Tran Tien Anh <nttan3kofile@gmail.com>
2023-11-14 00:26:57 +0700

    Restore App.js

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -16,7 +16,6 @@ import Home from './src/screens/Home';
 import Order from './src/screens/Order';
 import { AppContextProvider } from './src/utils/AppContext';
 import AppNavigator from './src/utils/AppNavigator';
-<<<<<<< Updated upstream
 import Slideshow from './src/component/Slideshow/Slideshow';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import ProductDetail from './src/screens/ProductDetail';
@@ -33,13 +32,9 @@ import DialogShopping from './src/screens/personStore/DialogShopping';
 import DetailPersonFedback from './src/screens/personStore/DetailPersonFedback';
 import DetailProduct from './src/screens/personStore/DetailProduct';
 import HomeStore from './src/screens/personStore/HomeStore';
-=======
-import CategoryManager from './src/screens/CategoryManager';
 import ProductProcess from './src/screens/personStore/ProductProcess';
 // import CensorshipProduct from './src/screens/CensorshipProduct';
 // import CensorshipDetailProduct from './src/screens/CensorshipDetailProduct';
-
->>>>>>> Stashed changes
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -48,7 +43,6 @@ import TestScreen from './src/screens/TestScreen';
 const Stack = createNativeStackNavigator()
 const App = () => {
   return (
-<<<<<<< Updated upstream
     <AppContextProvider>
       <NavigationContainer>
         <AppNavigator/>
@@ -57,22 +51,7 @@ const App = () => {
         {/* <UpdatePassword/> */}
       </NavigationContainer>
     </AppContextProvider>
-=======
-    // <AppContextProvider>
-    //   <NavigationContainer>
-    //     <AppNavigator />
-    //   </NavigationContainer>
-    // </AppContextProvider>
-
-    // <CensorshipDetailProduct/>
-    // <CategoryManager/>
-    // <CensorshipProduct/> // ko duyet: thong bao ko duyet, xoa | giao dien
-    <ProductProcess/>
->>>>>>> Stashed changes
   )
 }
-
-
-
 
 export default App;
